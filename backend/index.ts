283a506fb20472b5291be964ba13f502e1e2c977
Bruno Barboza <brunobarbozasilva@gmail.com>
2023-10-05 18:58:53 -0300

    Criando conexão com o banco de dados mongodb

--- backend/index.ts
+++ backend/index.ts
@@ -14,6 +14,43 @@ app.post('/', (request: Request, response: Response) => {
     response.send(responseBody);
 });
 
+import { Schema, model, connect } from 'mongoose';
+
+// 1. Create an interface representing a document in MongoDB.
+interface IUser {
+  name: string;
+  email: string;
+  avatar?: string;
+}
+
+// 2. Create a Schema corresponding to the document interface.
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  avatar: String
+});
+
+// 3. Create a Model for the "usuarios" collection in the "agenda" database.
+const User = model<IUser>('usuarios', userSchema); // 'usuarios' é o nome da coleção
+
+run().catch(err => console.log(err));
+
+async function run() {
+  // 4. Connect to MongoDB with the "agenda" database.
+  await connect('mongodb://127.0.0.1:27017/agenda'); // 'agenda' é o nome do banco de dados
+
+  const user = new User({
+    name: 'edson',
+    email: 'edson@initech.com',
+    avatar: 'https://i.imgur.com/dM7Thhn.png'
+  });
+  await user.save();
+
+  console.log(user.email); // 'bill@initech.com'
+}
+
+
+
 
 // const express = require('express');
 // const cors = require('cors');
