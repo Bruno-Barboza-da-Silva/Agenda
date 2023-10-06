c3fb865a8c998a84581850f9c91aa43f0242de3b
Bruno Barboza <brunobarbozasilva@gmail.com>
2023-10-05 22:16:55 -0300

    inclusão do email e senha no cadastro

--- frontend/src/pages/cadastro/cadastro.tsx
+++ frontend/src/pages/cadastro/cadastro.tsx
@@ -4,20 +4,35 @@ import axios from 'axios'
 function App() {
   // const navigate = useNavigate();
   
-  const [titulo, setName] = useState("")
+  const [nome, setName] = useState("")
 
   function changeName(ev: React.ChangeEvent<HTMLInputElement>) {
     setName(ev.target.value);
   }
 
+  const [email, setEmail] = useState("")
+  
+  function changeEmail(ev: React.ChangeEvent<HTMLInputElement>) {
+    setEmail(ev.target.value)
+  }
+  
+  const [senha, setSenha] = useState("")
+  
+  function changeSenha(ev: React.ChangeEvent<HTMLInputElement>) {
+    setSenha(ev.target.value)
+  }
+
+
+
   const post = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault(); // Evita o envio automático do formulário
     try {
       await axios.post('http://localhost:5000/cadastro', {
-        nome: titulo,
+        nome: nome,
+        email: email,
+        senha: senha
       });
       window.location.href = "/";
-      console.log("entrou");
     } catch (error) {
       console.error('Erro na solicitação POST:', error);
     }
@@ -28,14 +43,11 @@ function App() {
       <form onSubmit={post}>
         <h1>Cadastro</h1>
         <label htmlFor="nome">Nome:</label>
-        <input
-          type="text"
-          id="name"
-          placeholder="insira seu nome"
-          name="nome"
-          value={titulo}
-          onChange={(ev) => changeName(ev)}
-        />
+        <input type="text" id="name" placeholder="insira seu nome" name="nome" value={nome} onChange={(ev) => changeName(ev)}/>
+        <label htmlFor="email">E-mail:</label>
+        <input type="email" id="email" placeholder="insira seu e-mail" name="email" value={email} onChange={(ev) => changeEmail(ev)}/>
+        <label htmlFor="password">Senha:</label>
+        <input type="password" id="password" placeholder="insira sua senha" name="password" value={senha} onChange={(ev) => changeSenha(ev)}/>
         <button type='submit'>Enviar</button>
       </form>
     </>
